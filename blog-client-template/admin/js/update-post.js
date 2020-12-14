2690b26801f7e30a77f9b0edc80cdf08f6c79c20
dirren17 <71495689+dirren17@users.noreply.github.com>
2020-12-14 15:04:52 +0100

    updated post done

--- blog-client-template/admin/js/update-post.js
+++ blog-client-template/admin/js/update-post.js
@@ -28,17 +28,21 @@ function updatePostEvent() {
         e.preventDefault();
 
         let formData = new FormData(this);
-        let object = {content: formData.get('content')}
+        let object = {
+            title: formData.get('title'),
+            author: formData.get('author'),
+            content: formData.get('content')
+        }
     
         try {
-            await fetch('http://localhost:3000/posts' + urlParams.get('id'), {
+            await fetch('http://localhost:3000/posts/' + urlParams.get('id'), {
                 method: 'PATCH',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(object)
-            });
-    
+                body: JSON.stringify(object) 
+            }); 
+
             window.location.replace('index.html')
         } catch (message) {
             throw new Error(message);
