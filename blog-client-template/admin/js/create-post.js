9cfa2a69853efedf935a9f0749d4d6991af5d925
Emilia Lejonstig <emilia.lejonstig@gmail.com>
2020-12-14 13:56:14 +0100

    emilia branch created

--- blog-client-template/admin/js/create-post.js
+++ blog-client-template/admin/js/create-post.js
@@ -2,7 +2,6 @@ let form = document.getElementById('create-post');
 form.addEventListener('submit', createPost);
 
 
-
 async function createPost(e) {
     e.preventDefault();
 
@@ -18,7 +17,6 @@ async function createPost(e) {
     console.log(JSON.stringify(object));
 
 
-
     try {
         await fetch('http://localhost:3000/posts',  //länk kopplas till databas
             { method: 'POST',
@@ -27,6 +25,23 @@ async function createPost(e) {
         });
 
         window.location.replace('index.html') //gör att du kommer tillbaka och ser förstasidan med alla posts
+    } catch (message) {
+        throw new Error(message);
+    }
+}
+
+async function prefillForm() {
+    let urlParams = new URLSearchParams(window.location.search);
+    let postId = urlParams.get('id');
+
+    try{
+        let response = await fetch('http://localhost:3000/posts')
+        let data = await response.json();
+
+        document.getElementById('title-textarea').value = data.title;
+        document.getElementById('author-textarea').value = data.author;
+        document.getElementById('content-textarea').value = data.content;
+
     } catch (message) {
         throw new Error(message);
     }
